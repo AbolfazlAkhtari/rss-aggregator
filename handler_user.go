0271eb45a09cd85c275d6fd11e3e5bcae99713c3
Abolfazl Akhtari <abolfazl.akhtari42@gmail.com>
2024-01-05 20:37:19 +0330

    Added Auth functionality and a show user route

--- handler_user.go
+++ handler_user.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/google/uuid"
+	"gituhb.com/AbolfazlAkhtari/RSS/internal/auth"
 	"gituhb.com/AbolfazlAkhtari/RSS/internal/database"
 	"net/http"
 	"time"
@@ -31,6 +32,22 @@ func (apiCfg *apiConfig) handlerCreateUser(w http.ResponseWriter, r *http.Reques
 	})
 	if err != nil {
 		respondWithError(w, 400, fmt.Sprintf("Coudn't create a new user: %v", err))
+		return
+	}
+
+	respondWithJson(w, 201, dbUserToUser(user))
+}
+
+func (apiCfg *apiConfig) handlerGetUser(w http.ResponseWriter, r *http.Request) {
+	apikey, err := auth.GetApiKey(r.Header)
+	if err != nil {
+		respondWithError(w, 400, fmt.Sprintf("Auth err: %v", err))
+		return
+	}
+
+	user, err := apiCfg.DB.GetUserByApiKey(r.Context(), apikey)
+	if err != nil {
+		respondWithError(w, 404, fmt.Sprintf("Couldn't get User: %v", err))
 		return
 	}
 
